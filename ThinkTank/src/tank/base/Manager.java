c563b68f6c09fb972af2793b6fc15559cd3bce8f
MaheshGH <dmahesh46@gmail.com>
2015-03-14 20:59:16 +0530

    This is first commit.

---
+++ ThinkTank/src/tank/base/Manager.java
@@ -0,0 +1,14 @@
+package tank.base;
+
+
+	import org.openqa.selenium.WebDriver;
+
+	public interface Manager {
+		
+		WebDriver getDriver(String browser_name);
+
+		void setDriver(String browser);
+
+		void closeDriver(WebDriver driver);
+	}
+
